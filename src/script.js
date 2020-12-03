be8ac50124d0146b37895a17b20b6a3e1e3faf3a
Marcus Vinicius <marcus.conduru@gmail.com>
2020-12-03 20:33:56 -0300

    Adicionando html,js,css,bootstrap

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function alterarCor() {
+    document.getElementsByClassName('insert__select')[0].classList.toggle('focus__Red');
+    document.getElementsByClassName('insert__date')[0].classList.toggle('focus__Red');
+    document.getElementsByClassName('insert__text')[0].classList.toggle('focus__Red');
+    document.getElementsByClassName('insert__submit')[0].classList.toggle('insert__submit--red');
+}
