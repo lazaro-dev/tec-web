a08a707c86ca5781a522f116e1c2bc0f8502868b
Lázaro Sobral <lazaro.sobral@facape.br>
2020-12-03 22:02:18 -0300

    server

--- server.php
+++ server.php
@@ -14,8 +14,12 @@
     }
 
     if($op == 'read'){
-        $result = $conn->query('select * from users')->fetch_all();
-        // dd($result);
+        $result['credito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
+                                FROM contas 
+                                WHERE tipo_lancamento = 1')->fetch_all();
+        $result['debito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
+                                FROM contas 
+                                WHERE tipo_lancamento = 2')->fetch_all();        
         echo json_encode($result);
     }
 
@@ -26,3 +30,7 @@
         var_dump($var);
         die;
     }
+
+    // SELECT contas.id, contas.descricao, contas.data, contas.valor, tipo_lancamento.descricao 
+    // FROM `contas` 
+    // INNER JOIN tipo_lancamento ON contas.tipo_lancamento = tipo_lancamento.id
