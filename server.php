a14089b2ee673006265b083c159643455a85de02
Lázaro Sobral <lazaro.sobral@facape.br>
2020-12-03 23:24:59 -0300

    server

--- server.php
+++ server.php
@@ -14,12 +14,21 @@
     }
 
     if($op == 'read'){
-        $result['credito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
-                                FROM contas 
-                                WHERE tipo_lancamento = 1')->fetch_all();
-        $result['debito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
-                                FROM contas 
-                                WHERE tipo_lancamento = 2')->fetch_all();        
+
+        try {
+            $result['credito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
+                                    FROM contas 
+                                    WHERE tipo_lancamento = 1')->fetch_all(MYSQLI_ASSOC);
+            $result['debito'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
+                                    FROM contas 
+                                    WHERE tipo_lancamento = 2')->fetch_all(MYSQLI_ASSOC);
+            $result['status'] = 200;
+            $result['message'] = 'Success';
+        }catch(Exception $e){
+            $result['status'] = 500;
+            $result['message'] = 'Fail';
+        }
+        
         echo json_encode($result);
     }
 
@@ -34,3 +43,16 @@
     // SELECT contas.id, contas.descricao, contas.data, contas.valor, tipo_lancamento.descricao 
     // FROM `contas` 
     // INNER JOIN tipo_lancamento ON contas.tipo_lancamento = tipo_lancamento.id
+
+
+    // try {
+    //     $result['lancamentos'] = $conn->query('SELECT id, descricao, data, valor, tipo_lancamento 
+    //                             FROM contas')->fetch_all(MYSQLI_ASSOC);
+    // //   dd($result);
+    //   $result['status'] = 200;
+    //   $result['message'] = 'Success';
+    // }catch(Exception $e){
+    //     $result['status'] = 500;
+    //     $result['message'] = 'Fail';
+    // }
+    // echo json_encode($result);
